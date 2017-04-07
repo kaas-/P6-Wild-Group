748fadcd7a9af0442d4c6d58332ffe9a5efe54bb
Rasmus Christiansen <rall94.thekaas@gmail.com>
2017-04-07 16:05:10 +0200

    button test

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        //text = GetComponentInChildren<Text>();
+	}
+
+    public void buttonTest()
+    {
+        text.text = "button clicked";
+    }
+	
+}
