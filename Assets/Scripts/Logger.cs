82d5a67609da110657916f39a539c4990c311d53
Rasmus Christiansen <rall94.thekaas@gmail.com>
2017-04-25 11:46:12 +0200

    Initial setup for Logging

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Logger : MonoBehaviour {
+
+    private string filename;
+    private string path;
+
+    void StartLog ()
+    {
+
+        path = Application.persistentDataPath;
+    }
+
+}
